d0e9d4e44b9f397c16917f000b0d70963d17977a
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-05-10 09:45:02 +0530

    Style buttons

--- src/components/CvForm/FormButtons.js
+++ src/components/CvForm/FormButtons.js
@@ -4,9 +4,15 @@ class FormButtons extends Component {
   render() {
     return (
       <div className="form-btns">
-        <button onClick={this.props.showCv}>Show</button>
-        <button onClick={this.props.loadEg}>Load</button>
-        <button onClick={this.props.resetForm}>Reset</button>
+        <button onClick={this.props.showCv} className="show-btn">
+          Show
+        </button>
+        <button onClick={this.props.loadEg} className="load-btn">
+          Load
+        </button>
+        <button onClick={this.props.resetForm} className="reset-btn">
+          Reset
+        </button>
       </div>
     );
   }
