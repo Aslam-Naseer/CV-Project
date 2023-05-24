48cc32a3fb956cb3d722fe4dd2a93c1a6fc86c9f
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-05-24 19:49:21 +0530

    Add sign in and sign out

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,13 +1,52 @@
 import { Component } from "react";
 import "../signin.css";
 
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
 class SignIn extends Component {
   constructor(props) {
     super(props);
+
+    this.signIn = this.signIn.bind(this);
+    this.signOutUser = this.signOutUser.bind(this);
+    this.isUserSigned = this.isUserSigned.bind(this);
   }
 
+  isUserSigned = () => !!getAuth().currentUser;
+
+  signIn = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(getAuth(), provider);
+  };
+
+  signOutUser = () => {
+    signOut(getAuth());
+  };
+
+  signInBtn = (
+    <button className="signin" onClick={this.signIn}>
+      SignIn
+    </button>
+  );
+
+  signOutBtn = (
+    <button className="signin" onClick={this.signOutUser}>
+      Sign Out
+    </button>
+  );
+
   render() {
-    return <button className="signin">SignIn</button>;
+    return (
+      <div className="current-user">
+        {this.isUserSigned() ? getAuth().currentUser.displayName : ""}{" "}
+        {this.isUserSigned() ? this.signOutBtn : this.signInBtn}
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const app = initializeApp({
+  apiKey: "AIzaSyCrMSXUD3XPnlxakwXkYUnngyEfmusJ1sc",
+  authDomain: "cv-form-cv.firebaseapp.com",
+  projectId: "cv-form-cv",
+  storageBucket: "cv-form-cv.appspot.com",
+  messagingSenderId: "254784420060",
+  appId: "1:254784420060:web:d400bc9c4acb08ef555c19",
+  measurementId: "G-817C7D5R4S",
+});
+console.log(app);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+function initFirebaseAuth() {
+  onAuthStateChanged(getAuth(), authStateObserver);
+}
+
+function authStateObserver(user) {
+  console.log(user);
+}
+
+initFirebaseAuth();
